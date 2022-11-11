8c4f01fa1b10b0237ca64ce2cecb16f73b248706
jperez <jperez@106-20.info2>
2022-11-11 12:52:51 +0100

    comentarios edl 3 completos

--- EjerciciosT3-JuanPérez/src/ejercicios/Ejercicio3.java
+++ EjerciciosT3-JuanPérez/src/ejercicios/Ejercicio3.java
@@ -6,34 +6,37 @@ public class Ejercicio3 {
 
 	public static void main(String[] args) {
 
-		int numeroIntroducido;
+		int numeroIntroducido; //Declaro variable donde guardare el numero introducido por el usuario
 		
+		//Creo un Scanner, lo nombro dogma y lo importo
 		Scanner dogma = new Scanner(System.in);
 		
+		//Solicito un numero al usuario
 		System.out.print("Introduzca un numero que representara la base y la altura del triangulo: ");
 		
+		//Asigno el valor dado por el usuario a la variable numeroIntroducido mediante el Scanner
 		numeroIntroducido = dogma.nextInt();
 		
-		//For para haecr salto de linea, se completa al finalizar los bucles dentro de el
-		//valor inicial de variable i igual a 1 (1=primera linea o print)
-		//cuenta desde 1 hasta el numeroIntroducido
-		//suma +1 a variable i en cada iteracion
+		//Creo un for para hacer salto de linea, se completa tras finalizar los bucles dentro de el.
+		//valor inicial de variable i igual a 1 (1=primera linea o print).
+		//cuenta desde 1 hasta el numeroIntroducido.
+		//suma +1 a variable i en cada iteracion.
 		for (int i=1; i<=numeroIntroducido; i++) {
 
-			//for para añadir espacio, añade uno por iteracion, hace todas las iteraciones posibles antes de salir del bucle
-			//valor inicial de e igual a i, para introducir  antes de completar iteracion de bucle 
+			//for para añadir espacio, añade uno por iteracion, hace todas las iteraciones posibles antes de salir del bucle.
+			//valor inicial de e igual a i, antes de completar primera iteracion de bucle .
 			//cuenta desde e hasta un numero antes del numeroIntroducido,
-			//imprimiendo espacio un numero de veces igual al numeroIntroducido-1
-			//añade +1 a varaible e al final de iteracion
+			//imprimiendo espacio un numero de veces igual al numeroIntroducido-1.
+			//añade +1 a variable e al final de iteracion.
 			for (int e=i; e<numeroIntroducido; e++) {
 				System.out.print(" ");
 				}
 			
-			//for para añadir asterisco, añade uno por iteracion, hace todas las iteraciones posibles antes de salir del bucle
-			//valor inicial de e igual a i, para introducir  antes de completar iteracion de bucle 
-			//cuenta desde e hasta un numero antes del numeroIntroducido,
-			//imprimiendo espacio un numero de veces igual al numeroIntroducido-1
-			//añade +1 a varaible e al final de iteracion
+			//for para añadir asterisco, añade uno por iteracion, hace todas las iteraciones posibles antes de salir del bucle.
+			//valor inicial de e igual a 1.
+			//cuenta desde e hasta i,
+			//imprimiendo asterisco un numero de veces igual a i.
+			//añade +1 a variable e al final de iteracion.
 			for (int e=1; e<=i; e++) {
 			
 			System.out.print("* ");
@@ -44,6 +47,7 @@ public class Ejercicio3 {
 		
 		}
 		
+		//Cierro Scanner
 		dogma.close();
 
 	}
