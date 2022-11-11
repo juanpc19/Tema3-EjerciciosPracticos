ea5b8e23e650217f507473906050c0d7b8035cda
jperez <jperez@106-20.info2>
2022-11-11 10:47:09 +0100

    ej 3 ya esta bien completar comentarios

--- EjerciciosT3-JuanPérez/src/ejercicios/Ejercicio3.java
+++ EjerciciosT3-JuanPérez/src/ejercicios/Ejercicio3.java
@@ -7,8 +7,6 @@ public class Ejercicio3 {
 	public static void main(String[] args) {
 
 		int numeroIntroducido;
-
-		String asterisco="",espacio="";
 		
 		Scanner dogma = new Scanner(System.in);
 		
@@ -16,16 +14,36 @@ public class Ejercicio3 {
 		
 		numeroIntroducido = dogma.nextInt();
 		
-		for (int i=0; i<=numeroIntroducido; i++) {
-			
-			System.out.println("");
-			
-			System.out.println(asterisco);
-			
-			asterisco+="* "; 
-			//espacio+=" ";
-		}
+		//For para haecr salto de linea, se completa al finalizar los bucles dentro de el
+		//valor inicial de variable i igual a 1 (1=primera linea o print)
+		//cuenta desde 1 hasta el numeroIntroducido
+		//suma +1 a variable i en cada iteracion
+		for (int i=1; i<=numeroIntroducido; i++) {
 
+			//for para añadir espacio, añade uno por iteracion, hace todas las iteraciones posibles antes de salir del bucle
+			//valor inicial de e igual a i, para introducir  antes de completar iteracion de bucle 
+			//cuenta desde e hasta un numero antes del numeroIntroducido,
+			//imprimiendo espacio un numero de veces igual al numeroIntroducido-1
+			//añade +1 a varaible e al final de iteracion
+			for (int e=i; e<numeroIntroducido; e++) {
+				System.out.print(" ");
+				}
+			
+			//for para añadir asterisco, añade uno por iteracion, hace todas las iteraciones posibles antes de salir del bucle
+			//valor inicial de e igual a i, para introducir  antes de completar iteracion de bucle 
+			//cuenta desde e hasta un numero antes del numeroIntroducido,
+			//imprimiendo espacio un numero de veces igual al numeroIntroducido-1
+			//añade +1 a varaible e al final de iteracion
+			for (int e=1; e<=i; e++) {
+			
+			System.out.print("* ");
+		
+			}
+			
+		System.out.println("");
+		
+		}
+		
 		dogma.close();
 
 	}
--- EjerciciosT3-JuanPérez/src/ejercicios/Ejercicio7.java
+++ EjerciciosT3-JuanPérez/src/ejercicios/Ejercicio7.java
@@ -1,5 +1,7 @@
 package ejercicios;
 
+import java.util.Scanner;
+
 public class Ejercicio7 {
 
 	public static void main(String[] args) {
