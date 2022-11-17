17e7ca04e91f3272218a5d6b3a98e0613a504322
jperez <jperez@106-20.info2>
2022-11-17 14:43:36 +0100

    ej1

--- EjerciciosT3-JuanPérez/src/ejercicios/Ejercicio1.java
+++ EjerciciosT3-JuanPérez/src/ejercicios/Ejercicio1.java
@@ -5,108 +5,84 @@ import java.util.Scanner;
 public class Ejercicio1 {
 
 	public static void main(String[] args) {
+
+		int horas;//Declaro variable donde guardare las horas
 		
-		int horas;
-		int minutos;
-		int segundos;
-		int incrementoSegundos;
+		int minutos;//Declaro variable doned guardare los minutos
 		
+		int segundos;//Declaro varible donde guardare los segundos
+		
+		int incrementoSegundos;//Declaro variable donde guardare los segundos incrementados 
+		
+		//Creo el Scanner, lo nombro dogma y lo importo
 		Scanner dogma = new Scanner(System.in);
 		
+		//Solicito al usuario un valor para variable horas
 		System.out.print("Introduzca las horas: ");
 		
+		//Asigno a variable horas un valor mediante uso del Scanner
 		horas = dogma.nextInt();
 		
+		//Solicito al usuario un valor para variable minutos
 		System.out.print("Introduzca los minutos: ");
 		
+		//Asigno a variable minutos un valor mediante uso del Scanner
 		minutos = dogma.nextInt();
 		
-		System.out.print("Introduzca los segundos: ");
-	
-		segundos = dogma.nextInt();
-		
-		System.out.print("En cuantos segundos desea incrementar la hora?");
-		
-		incrementoSegundos = dogma.nextInt();
-		
-		segundos += incrementoSegundos;
-		
-		while (segundos>59) {
-			
-			minutos +=1;
-			segundos -=60;
-		}
-	
-		while (minutos>59) {
-			
-			horas +=1;
-			minutos -=60;
-		}
-		
-		while (horas>23) {
-			
-			horas = 0;
-		}
-	
-		System.out.print("La nueva hora es: " + horas + " : " + minutos + " : " + segundos + " : ");
-		
-		dogma.close();
-
-	}
-
-}
-
-		int horas;
-		int minutos;
-		int segundos;
-		int incrementoSegundos;
-		
-		Scanner dogma = new Scanner(System.in);
-		
-		System.out.print("Introduzca las horas: ");
-		
-		horas = dogma.nextInt();
-		
-		System.out.print("Introduzca los minutos: ");
-		
-		minutos = dogma.nextInt();
-		
+		//Solicito al usuario un valor para variable segundos
 		System.out.print("Introduzca los segundos: ");
 		
+		//Asigno a variable segundos un valor mediante uso del Scanner
 		segundos = dogma.nextInt();
 		
+		//Solicito al usuario un valor para variable incrementoSegundos
 		System.out.print("En cuantos segundos desea incrementar la hora?");
 		
+		//Asigno a variable incrementoSegundos un valor mediante uso del Scanner
 		incrementoSegundos = dogma.nextInt();
 		
+		//Creo un bucle para extraer los segundos de incrementoSegundos
 		
+		//No doy valor inicial a la variable que usara el bucle (incrementoSegundos)
+		//porque ya tiene su valor dado por el usuario
 		
-		for (int i=1; i<incrementoSegundos;  i++) {
+		//Establezco condicion incrementoSegundos mayor que 0 que mantendra al bucle en ejecucion mientras se cumpla
+		
+		//Doy al decremento de variable incrementoSegundos un valor de -1 
+		//que restaraal final de iteracion a variable incrementoSegundos
+		for (; incrementoSegundos>0;  incrementoSegundos--) {
 			
-		
-			if (incrementoSegundos+segundos<=59) {
-				segundos+=incrementoSegundos;
-			}
-			else if (incrementoSegundos+segundos>59) {
-				incrementoSegundos-=60;
+			//Sumo  +1 a segundos en cada iteracion
+			segundos++;
+			
+			//Establezco condicion si segundos igual a 60 
+			if (segundos==60) {
+				//De cumplirse la condicion anterior dara a segundos un valor igual a 0
 				segundos=0;
+				//De cumplirse la condicion anterior sumara a minutos +1
 				minutos++;
 				
 			}
-			else if (minutos>59) {
-				incrementoSegundos-=3600;
+			//Establezco condicion si minutos mayor a 59
+			if (minutos>59) {
+				//De cumplirse la condicion anterior dara a minutos un valor igual a 0
 				minutos=0;
+				//De cumplirse la condicion anterior sumara a horas +1
 				horas++;
-				
+			
 			}
-			else if (horas>23) {
+			//Establezco condicion si horas mayor a 23
+			 if (horas>23) {
+				//De cumplirse la condicion anterior doy a horas valor igual a 0
 				horas=0;
 			}
 		}
 			
-			
+		//Imprimo mensaje por pantalla 
+		System.out.print("La nueva hora es: " + horas + " : " + minutos + " : " + segundos);
 		
-		System.out.print("La nueva hora es: " + horas + " : " + minutos + " : " + segundos + " : ");
-		
+		//Cierro Scanner
 		dogma.close();
-
+		
+	}
+}
